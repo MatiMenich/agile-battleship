c94a1c01ea1216bd6b1f8a29bbe1be86f6e8af69
Matías Menich <matimenich@gmail.com>
2015-10-05 19:44:34 -0300

    Initial commit

---
+++ javascripts/app/Cell.js
@@ -0,0 +1,95 @@
+var Cell = Backbone.Model.extend({
+  defaults: {
+    state: "empty"
+  },
+  fire: function() {
+    this.trigger("fire", this);
+  },
+  updateState: function() {
+    if (this.has("boat")) {
+      this.get("boat").hit(this);
+      this.set("state", "hit");
+    } else {
+      this.set("state", "miss");
+    }
+  }
+});
+
+var CellView = Backbone.View.extend({
+  tagName: "td",
+  className: "cell",
+  events: {
+    "click": "fire"
+  },
+  initialize: function() {
+    _.bindAll(this, "renderBoat", "updateState", "disable");
+    this.model.bind("change:boat", this.renderBoat);
+    this.model.bind("change:state", this.updateState);
+    this.model.bind("change:disabled", this.disable);
+  },
+  render: function() {
+		cell_x = this.model.get("x");
+		cell_y = this.model.get("y");
+	
+		this.$el.attr("id", "cell-" + cell_x + "-" + cell_y);
+		// this.$el.html();
+
+		if(cell_x > 10){
+			this.disable();
+			this.$el.addClass("cell-title-right");
+		}
+		
+		if(cell_y > 10){
+			this.disable();
+			this.$el.addClass("cell-title-bottom");
+		}
+		
+		if(cell_x === 0) {
+			this.disable();
+			this.$el.addClass("cell-title-left");
+			if(cell_y>0 && cell_y<=10 ){
+				this.$el.html(cell_y);		  
+			}
+		}
+
+		A_charcode = "A".charCodeAt(0);
+		if(cell_y === 0) {
+			this.disable();
+			this.$el.addClass("cell-title-top");
+			if(cell_x>0 && cell_x <= 10){
+				this.$el.html(String.fromCharCode(A_charcode+cell_x-1));
+			}
+		}
+    this.renderBoat();
+    return this;
+  },
+  renderBoat: function() {
+    if (this.model.has('boat')) {
+      if (this.model.get("boat").get("visible")) {
+        this.$el.addClass("showBoat");
+        this.$el.addClass(this.model.get("boat").get("type"));
+      }
+      this.model.get('boat').bind("change:visible", this.renderBoat);
+    }
+  },
+  updateState: function() {
+    if (this.model.get("state") == "hit") {
+      // this.$el.addClass("hit");
+      this.$el.html("<img class='hit marker animated bounceIn' src='images/1331900690_fire.png'/>");
+    } else if (this.model.get("state") == "miss") {
+      // this.$el.html("<i class='miss marker animated flipInX fa fa-times fa-2x text-danger' ><i/>");
+      this.$el.html("<img class='miss marker animated flipInX' src='images/1331900805_cross.png'/>");
+      // this.$el.addClass("miss");
+    }
+  },
+  fire: function() {
+    // this.$el.addClass("target");
+    this.$el.html("<img class='target marker animated slideInDown' src='images/1331901174_bullet_red.png'/>");
+    // this.$el.html("<i class='target marker animated slideInDown fa fa-bullseye fa-2x text-info' ><i/>");
+    this.model.fire();
+    this.$el.unbind("click");
+  },
+  disable: function() {
+    this.$el.unbind("click");
+  }
+});
---
+++ javascripts/app/Game.js
@@ -0,0 +1,129 @@
+var Game = Backbone.Model.extend({
+  defaults: {
+    shotsPerIteration: 40,
+    maxShots: 40,
+    costPerShot: 10000,
+    sunkenBoatCellReward: 50000,
+    fleet: []
+  },
+  initialize: function(args) {
+    _.bindAll(this, "sunkenBoat", "shotFired");
+    this.set("shotsRemainingForIteration", args.shotsPerIteration);
+    this.set("shotsRemainingForGame", this.get("maxShots"));
+    this.set("funds", this.get("maxShots") * this.get("costPerShot"));
+    
+    this.sunken = 0;
+    this.set("board", new Board());
+    this.get("board").bind("fire", this.shotFired);
+
+    var directions = ["vertical", "horizontal"];
+
+    var self = this;
+    var board = this.get("board");
+    if(this.get("fleet").length === 0) {
+      _(["aircraft-carrier","battleship","submarine","cruiser","destroyer"]).each(function(type) {
+        var placed = false;
+        while(!placed) {
+          var boat = new Boat({x: self.random(board.get("gridSize").x + 1), y: self.random(board.get("gridSize").y + 1), direction: directions[self.random(2)], type: type, visible: false});
+          if(board.validBoatPlacement(boat)) {
+            self.addBoat(boat);
+            placed = true;
+          }
+        }
+      });
+    } else {
+      _(this.get("fleet")).each(function(boat) {
+        self.addBoat(boat);
+      });
+    }
+  },
+  random: function(max) {
+    return Math.floor(Math.random()*max);
+  },
+  destroy: function() {
+    this.get("board").destroy();
+    Game.__super__.destroy();
+  },
+  addBoat: function(boat) {
+    boat.bind("sunken", this.sunkenBoat);
+    this.get("board").addBoat(boat);
+  },
+  shotFired: function() {
+    this.set("shotsRemainingForIteration", this.get("shotsRemainingForIteration") - 1);
+    this.set("funds", this.get("funds") - this.get("costPerShot"));
+    this.set("shotsRemainingForGame", this.get("shotsRemainingForGame") - 1);
+
+    if (this.get("shotsRemainingForIteration") <= 0) {
+      this.set("shotsRemainingForIteration", this.get("shotsPerIteration"));
+      this.get("board").showFeedback();
+    }
+
+    if (this.get("shotsRemainingForGame") <= 0) {
+      this.endGame();
+    }
+  },
+  sunkenBoat: function(boat) {
+    this.sunken++;
+    this.set("funds", this.get("funds") + boat.length() * this.get("sunkenBoatCellReward"));
+    if(this.fleetDestroyed() && this.get("shotsRemainingForGame") > 0) {
+      this.endGame();
+    }
+  },
+  fleetDestroyed: function() {
+    return this.sunken >= this.get("board").fleetSize();
+  },
+  endGame: function() {
+    if (!this.has("endGameState")) {
+      if (this.fleetDestroyed()) {
+        this.set("endGameState", "win");
+      } else {
+        this.set("endGameState", "lose");
+      }
+
+      this.get("board").disable();
+      this.get("board").showFleet();
+    }
+  }
+});
+
+var GameView = Backbone.View.extend({
+  initialize: function(args) {
+    _.bindAll(this, "updateShotsRemainingForGame", "updateShotsRemainingForIteration", "updateEndGameState", "updateFunds");
+    $(".message").addClass('hidden');
+    $(".stats").removeClass('hidden');
+    this.model.bind("change:shotsRemainingForGame", this.updateShotsRemainingForGame);
+    this.model.bind("change:shotsRemainingForIteration", this.updateShotsRemainingForIteration);
+    this.model.bind("change:funds", this.updateFunds);
+    this.model.bind("change:endGameState", this.updateEndGameState);
+  },
+  render: function() {
+    this.boardView = new BoardView({model: this.model.get("board")});
+    $("#container").append(this.boardView.render().el);
+    this.updateShotsRemainingForGame();
+    this.updateShotsRemainingForIteration();
+    this.updateFunds();
+    $("#endGameResult").html("");
+    return this;
+  },
+  updateShotsRemainingForGame: function() {
+    $("#totalShotsRemaining").html(this.model.get("shotsRemainingForGame"));
+  },
+  updateShotsRemainingForIteration: function() {
+    $("#shotsRemainingForIteration").html(this.model.get("shotsRemainingForIteration"));
+  },
+  updateFunds: function() {
+    var funds = this.model.get("funds");
+    $("#funds").html(this.euros(funds));
+  },
+  updateEndGameState: function(model, endGameState) {
+    var diff = this.model.get("funds") - this.model.get("maxShots") * this.model.get("costPerShot");
+    if (endGameState === "lose") {
+      $("#endGameResult").html('<div class="alert alert-info alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-flag-checkered"></i> ¡Juego Finalizado!</strong> Lograste ' + this.euros(diff) + '</div>');
+    } else {
+      $("#endGameResult").html('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-trophy"></i> ¡Ganaste!</strong> Lograste ' + this.euros(diff) + '</div>');
+    }
+  },
+  euros: function(amount) {
+    return "US&dollar; " + $.formatNumber(amount, {format:"#,##0", locale:"nl"});
+  }
+});
