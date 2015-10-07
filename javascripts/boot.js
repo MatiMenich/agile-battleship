2228ae410eaacdf25a3e6d93818f97125dec7f65
Matías Menich <matimenich@gmail.com>
2015-10-06 21:06:28 -0300

    Start games with different previously defined strategies

--- javascripts/boot.js
+++ javascripts/boot.js
@@ -5,7 +5,23 @@ $(function() {
     if (game) {
       game.destroy();
     }
-    game = new Game({shotsPerIteration: $("#shotsPerIteration").val()});
+    game = new Game({ shotsPerIteration: $("#shotsPerIteration").val() });
+    new GameView({model: game}).render();
+  });
+
+  $("#newDeterministicGame").bind("click", function() {
+    if (game) {
+      game.destroy();
+    }
+    game = new Game({ shotsPerIteration: 40 });
+    new GameView({model: game}).render();
+  });
+
+  $("#newAdaptativeGame").bind("click", function() {
+    if (game) {
+      game.destroy();
+    }
+    game = new Game({ shotsPerIteration: 1 });
     new GameView({model: game}).render();
   });
 });
